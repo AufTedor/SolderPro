a67769f9b85db2b019ef6f5e5df8493811c1571e
Houston <roman.martseniuk.developer@gmail.com>
2023-07-01 14:15:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function ToggleMenu() {
+   document.getElementById("nav-menu").classList.toggle('active');
+   document.getElementById("open-burger").classList.toggle('active');
+   document.getElementById("close-burger").classList.toggle('active');
+}
+
+function ToggleCheck() {
+   document.getElementById("checkbox-1").classList.toggle("checked");
+   document.getElementById("checkbox-2").classList.toggle("checked");
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "grid";
+}
